1591e2ba94f62d4f97e7cbfc5f106d1fb6984024
Rehan Sajid <rehansajid.dev@gmail.com>
2016-09-26 11:32:23 +0500

    Added the image view(property to VC) to PhotosView Controller and storyboard

---
+++ Photorama/PhotosViewController.swift
@@ -0,0 +1,14 @@
+//
+//  PhotosViewController.swift
+//  Photorama
+//
+//  Created by Rehan Sajid on 26/09/2016.
+//  Copyright © 2016 Rehan Sajid. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+        @IBOutlet var imageView: UIImageView!
+        
+}
